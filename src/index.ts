e6551f51edaef5c07fd4a6a9e8ec7932bf16d3e3
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-02-03 23:46:00 +0100

    Remove app Provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import Koa from "koa";
+import jwt from "koa-jwt";
+import bodyParser from "koa-bodyparser";
+import helmet from "koa-helmet";
+import cors from "@koa/cors";
+import winston from "winston";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
+
+import { logger } from "./provider/logger";
+import { config } from "./provider/config";
+import { cron } from "./provider/cron";
+import { publicRouter, protectedRouter } from "./provider/routes";
 import { cpus } from "os";
 import cluster from "cluster";
 
 // import App from "./providers/App";
 // import NativeEvent from "./exception/NativeEvent";
 
-const CPUS: any = cpus();
+const CPUS: number = cpus().length;
+console.log(`Starting at port ${config.port}`);
 
 if (cluster.isMaster) {
   console.log(`This machine has ${CPUS} CPUs.`);
@@ -23,46 +37,52 @@ if (cluster.isMaster) {
     console.log("Starting a new worker...");
     cluster.fork();
   });
-  /**
-   * Catches the process events
-   */
-  // NativeEvent.process();
-  /**
-   * Clear the console before the app runs
-   */
-  // App.clearConsole();
-  /**
-   * Load Configuration
-   */
-  // App.loadConfiguration();
-  /**
-   * Find the number of available CPUS
-   */
-  /**
-   * Fork the process, the number of times we have CPUs available
-   */
-  // CPUS.forEach(() => cluster.fork());
-  /**
-   * Catches the cluster events
-   */
-  // NativeEvent.cluster(cluster);
-  /**
-   * Loads the Queue Monitor iff enabled
-   */
-  // App.loadQueue();
-  /**
-   * Run the Worker every minute
-   * Note: we normally start worker after
-   * the entire app is loaded
-   */
-  // setTimeout(() => App.loadWorker(), 1000 * 60);
 } else {
-  /**
-   * Run the Database pool
-   */
-  // App.loadDatabase();
-  /**
-   * Run the Server on Clusters
-   */
-  // App.loadServer();
+  // create connection with database
+  // note that its not active database connection
+  // TypeORM creates you connection pull to uses connections from pull on your requests
+  createConnection({
+    type: "postgres",
+    url: config.databaseUrl,
+    synchronize: true,
+    logging: false,
+    entities: config.dbEntitiesPath,
+  })
+    .then(async () => {
+      const app = new Koa();
+
+      // Provides important security headers to make your app more secure
+      app.use(helmet());
+
+      // Enable cors with default options
+      app.use(cors());
+
+      // Logger middleware -> use winston as logger (logging.ts with config)
+      app.use(logger(winston));
+
+      // Enable bodyParser with default options
+      app.use(bodyParser());
+
+      // these routes are NOT protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
+      app.use(publicRouter.routes()).use(publicRouter.allowedMethods());
+
+      // JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
+      // do not protect swagger-json and swagger-html endpoints
+      app.use(
+        jwt({ secret: config.jwtSecret }).unless({ path: [/^\/swagger-/] })
+      );
+
+      // These routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
+      app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
+
+      // Register cron job to do any action needed
+      cron.start();
+
+      app.listen(config.port);
+
+      // eslint-disable-next-line no-console
+      console.log(`Server running on port ${config.port}`);
+    })
+    // eslint-disable-next-line no-console
+    .catch((error: string) => console.log("TypeORM connection error: ", error));
 }
