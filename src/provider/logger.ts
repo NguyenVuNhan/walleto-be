648d59bc215eb2ca309b405579c62db30f8c912f
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-02-05 23:28:31 +0100

    Improve logging

--- src/provider/logger.ts
+++ src/provider/logger.ts
@@ -1,17 +1,22 @@
 import { config } from "./config";
 import winston, { transports, format } from "winston";
+import "winston-daily-rotate-file";
 import * as path from "path";
 
 winston.configure({
   level: config.debugLogging ? "debug" : "info",
   transports: [
-    //
     // - Write all logs error (and below) to `error.log`.
-    new transports.File({
-      filename: path.resolve(__dirname, "../../error.log"),
-      level: "error",
+    new transports.DailyRotateFile({
+      filename: path.resolve(__dirname, "../../.logs/%DATE%.log"),
+      datePattern: "DD-MM-yyyy",
+      format: format.combine(
+        format.timestamp(),
+        format.printf(
+          (info) => `${info.timestamp} [${info.level}]: ${info.message}`
+        )
+      ),
     }),
-    //
     // - Write to all logs with specified level to console.
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
